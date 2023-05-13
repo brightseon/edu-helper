81bdc2163816055fece4f956728594a081cab62a
brightseon <public.seon@gmail.com>
2023-05-14 01:23:28 +0900

    feat: 예외 처리

--- main.py
+++ main.py
@@ -14,23 +14,28 @@ PASSWORD = os.environ.get('password')
 
 
 def main():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get(URL)
+    try:
+        driver = webdriver.Chrome(service=Service(
+            ChromeDriverManager().install()))
+        driver.get(URL)
 
-    for win in driver.window_handles[1:]:
-        driver.switch_to.window(win)
-        driver.close()
+        for win in driver.window_handles[1:]:
+            driver.switch_to.window(win)
+            driver.close()
 
-    driver.switch_to.window(driver.window_handles[0])
+        driver.switch_to.window(driver.window_handles[0])
 
-    id_box = driver.find_element(by=By.ID, value='userInputId')
-    password_box = driver.find_element(by=By.ID, value='userInputPw')
-    id_box.send_keys(ID)
-    password_box.send_keys(PASSWORD)
+        id_box = driver.find_element(by=By.ID, value='userInputId')
+        password_box = driver.find_element(by=By.ID, value='userInputPw')
+        id_box.send_keys(ID)
+        password_box.send_keys(PASSWORD)
 
-    time.sleep(3)
+        time.sleep(3)
 
-    driver.quit()
+        driver.quit()
+    except Exception as e:
+        print(e)
+        driver.quit()
 
 
 if __name__ == '__main__':
